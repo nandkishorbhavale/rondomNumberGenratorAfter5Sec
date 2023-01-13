232aefed1e97229c8a07b7a563cc281112f2871e
Nandkishor Bhavale <103604651+nandkishorbhavale@users.noreply.github.com>
2023-01-13 16:38:18 +0530

    Add files via upload

---
+++ RondomNumber.java
@@ -0,0 +1,17 @@
+public class RondomNumber {
+    public static void main(String[] args) {
+        int min = 1;
+        int max = 6;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Random value of type int between " + min + " to " + max + ":");
+            int b = (int) (Math.random() * (max - min + 1) + min);
+            System.out.println(b);
+            try {
+                Thread.sleep(5000); // 5000ms = 5s
+            } catch (InterruptedException ex) {
+
+                System.exit(0);
+            }
+        }
+    }
+}
